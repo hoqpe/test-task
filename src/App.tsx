a0241f8d298a4f64f45fb70d8cbecc745cda4c02
Alexander Medvedev <hoqpe@yandex.ru>
2021-05-16 11:38:31 +0300

    Запрос книг и разбивка по табам

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
+import { useState, useEffect, useMemo } from 'react'
+import { Book, Tab, TabName } from './types'
 import { Panel } from './components/Panel'
 import { BookCard } from './components/BookCard'
 
+type BooksTabs = Record<Book['id'], TabName>
+
+class BooksTab implements Tab {
+  items: Book[] = []
+
+  constructor (
+    public name: TabName,
+    public text: string,
+  ) {}
+}
+
+const booksUrl = 'https://raw.githubusercontent.com/lastw/test-task/master/data/10-items.json'
+
+const fetchBooks = async (): Promise<{ items: Book[] }> => {
+  const res = await window.fetch(booksUrl)
+  return await res.json()
+}
+
 const App = () => {
+  const [books, setBooks] = useState<Book[]>([])
+  const [booksTabs] = useState<BooksTabs>()
+
+  const tabs = useMemo(() => {
+    const tabs = {
+      toRead: new BooksTab('toRead', 'To read'),
+      inProgress: new BooksTab('inProgress', 'In progress'),
+      done: new BooksTab('done', 'Done')
+    }
+
+    if (!booksTabs) {
+      tabs.toRead.items = books
+      return tabs
+    }
+
+    for (const book of books) {
+      const bookStatus = booksTabs[book.id] ?? 'toRead'
+      tabs[bookStatus].items.push(book)
+    }
+
+    return tabs
+  }, [books, booksTabs])
+
+  const tabsList = useMemo(() => Object.values(tabs), [tabs])
+
+  useEffect(() => {
+    fetchBooks().then(({ items }) => setBooks(items))
+  }, [])
+
   return (
     <Panel
-      tabs={[
-        {
-          name: 'toRead',
-          text: 'To read',
-          items: []
-        },
-        {
-          name: 'inProgress',
-          text: 'In progress',
-          items: []
-        },
-        {
-          name: 'done',
-          text: 'Donew',
-          items: []
-        }
-      ]}
+      tabs={tabsList}
       selectedTabName="toRead"
     >
-      <BookCard
-        id="id-1"
-        title="title"
-        author="author"
-        description="description"
-        tags={['tag-1', 'tag-2']}
-      />
+      {books.map(book => (
+        <BookCard key={book.id} {...book} />
+      ))}
     </Panel>
   )
 }
